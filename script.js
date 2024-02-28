ab4cccd8a732a08a3ff994e4de972da338eb1e43
ANKUSH KUMAR <77243849+Kankush18@users.noreply.github.com>
2024-02-28 14:13:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let browserName=navigator.appName;
+let version=navigator.appVersion;
+
+let infoBox=document.getElementById('browser-info');
+infoBox.textContent="You are using " + browserName + " version " + version
