acff35747dd02e31c98cf003acdc04df96f08012
ANKUSH KUMAR <77243849+Kankush18@users.noreply.github.com>
2024-02-28 16:35:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 //your JS code here. If required.
-var newElement = document.createElement("div");
-
-
-newElement.id = "browser-info";
+let newElement=document.createElement('div');
+newElement.id='browser-info';
 let browserName=navigator.appName;
 let version=navigator.appVersion;
-newElement.textContent = "You are using " + browserName + " version " + version;
-
-
+newElement.innerText=`You are using  ${browserName } version   ${version}"`;
 document.body.appendChild(newElement);
